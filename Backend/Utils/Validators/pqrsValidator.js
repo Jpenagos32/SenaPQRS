fddc0d8e97714375747897719f757d27ce917af9
jpenagos32 <jpenagosdev@gmail.com>
2024-06-21 23:37:46 -0500

    Arreglar la validacion

--- Backend/Utils/Validators/pqrsValidator.js
+++ Backend/Utils/Validators/pqrsValidator.js
@@ -9,9 +9,9 @@ const putPqrsValidator = z.object({
   ]),
   correo: z.string().email(),
   nombre_completo: z.string().min(3).max(80),
-  tipo_identificacion: z.enum(['cedula de ciudadania', 'pasaporte', 'nit']),
+  tipo_identificacion: z.enum(['cedula', 'pasaporte', 'nit']),
   numero_identificacion: z.string(),
-  descripcion: z.string().min(20)
+  descripcion: z.string()
 })
 
 export function validatePutPqrs (object) {
